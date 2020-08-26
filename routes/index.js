0dce9019eb055fe784af690fd456f9132d8c450a
dante <dantinr@gmail.com>
2020-08-26 14:31:49 +0800

    中间件测试

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const checkLogin = require('../middleware/checkLogin')     //验证登录中间件
 
+router.use(checkLogin());
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'API' });
