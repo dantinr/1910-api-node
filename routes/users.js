7cb18a3e7fb072fefc5c77cdd016333a0a9230b2
dante <dantinr@gmail.com>
2020-08-26 14:48:57 +0800

    TODO 注册 登录

--- routes/users.js
+++ routes/users.js
@@ -6,8 +6,14 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// 注册
 router.post('/reg', function(req, res, next) {
-  res.send('111111');
+  // TODO 注册逻辑
+});
+
+// 登录
+router.post('/login', function(req, res, next) {
+  // TODO 登录逻辑
 });
 
 module.exports = router;
