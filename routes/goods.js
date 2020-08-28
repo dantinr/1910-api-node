b9ae1a84d01eeabb15727e95ccde1566e4a789ef
dante <dantinr@gmail.com>
2020-08-28 12:39:00 +0800

    首页接口

--- routes/goods.js
+++ routes/goods.js
@@ -31,13 +31,19 @@ router.get('/home', function(req, res, next) {
     errno : 0,
     msg: 'ok',
     data: {
-      list: [
+      list: [   //商品列表
         {goods_id:1234,goods_name:"IphoneX1111",goods_price:123.45},
         {goods_id:2345,goods_name:"IphoneX2222",goods_price:123.45},
         {goods_id:4567,goods_name:"IphoneX3333",goods_price:123.45},
         {goods_id:5678,goods_name:"IphoneX4444",goods_price:123.45},
         {goods_id:7890,goods_name:"IphoneX5555",goods_price:123.45},
         {goods_id:9999,goods_name:"IphoneX6666",goods_price:123.45},
+      ],
+      slider: [   //轮播图
+        {id:123,src:'/img/banner1.png',href:'http://www.baidu.com'},
+        {id:234,src:'/img/banner2.png',href:'http://www.baidu.com'},
+        {id:345,src:'/img/banner3.png',href:'http://www.baidu.com'},
+        {id:456,src:'/img/banner4.png',href:'http://www.baidu.com'},
       ]
     }
   };
